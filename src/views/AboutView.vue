47ffb4ab058452915cd66afe87a4b6ab39d31240
Shion Ichikawa <shion1305@gmail.com>
2022-07-15 05:18:25 +0900

    🎉 Initial commit
    implemented basic function in HomeView.vue
    loading image, reloading cropped image

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,119 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div id="app">
+    <cropper ref="cropper" class="cropper" @change="change" :src="image.src"
+    />
+    <div class="button-wrapper">
+      <button class="button" @click="$refs.file.click()">
+        <input
+            type="file"
+            ref="file"
+            @change="uploadImage($event)"
+            accept="image/*"
+        />
+        Upload image
+      </button>
+      <button class="button" @click="cropImage()">Crop image</button>
+    </div>
   </div>
 </template>
+
+<script>
+import {defineComponent} from "vue";
+import {Cropper} from "vue-advanced-cropper";
+import "vue-advanced-cropper/dist/style.css";
+
+export default defineComponent({
+  components: {
+    Cropper,
+  },
+  data() {
+    return {
+      image: {
+        src:
+            "https://images.unsplash.com/photo-1619737307100-55b82496fcda?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+        type: "image/jpg",
+      },
+    };
+  },
+  methods: {
+    change(result) {
+      console.log(result);
+    },
+    cropImage() {
+      const result = this.$refs.cropper.getResult();
+      const newTab = window.open();
+      newTab.document.body.innerHTML = `<img src="${result.canvas.toDataURL(
+          this.image.type
+      )}"></img>`;
+    },
+    uploadImage(event) {
+      /// Reference to the DOM input element
+      const {files} = event.target;
+      // Ensure that you have a file before attempting to read it
+      if (files && files[0]) {
+        // 1. Revoke the object URL, to allow the garbage collector to destroy the uploaded before file
+        if (this.image.src) {
+          URL.revokeObjectURL(this.image.src);
+        }
+        // 2. Create the blob link to the file to optimize performance:
+        const blob = URL.createObjectURL(files[0]);
+
+        // 3. Update the image. The type will be derived from the extension and it can lead to an incorrect result:
+        this.image = {
+          src: blob,
+          type: files[0].type,
+        };
+      }
+    },
+  },
+  unmounted() {
+    // Revoke the object URL, to allow the garbage collector to destroy the uploaded before file
+    if (this.image.src) {
+      URL.revokeObjectURL(this.image.src);
+    }
+  },
+});
+</script>
+
+<style lang="scss">
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.cropper {
+  max-height: 500px;
+}
+
+.button-wrapper {
+  display: flex;
+  justify-content: center;
+  margin-top: 17px;
+}
+
+.button {
+  color: white;
+  font-size: 16px;
+  padding: 10px 20px;
+  width: 100%;
+  background: #151515;
+  cursor: pointer;
+  transition: background 0.5s;
+  border: none;
+
+  &:not(:last-of-type) {
+    margin-right: 10px;
+  }
+
+  &:hover {
+    background: #2F2F2F;
+  }
+
+  input {
+    display: none;
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,64 @@
+<script setup lang="ts">
+import 'vue-advanced-cropper/dist/style.css';
+import {Cropper} from "vue-advanced-cropper";
+import {ref} from "vue";
+
+let img = ref({
+  src:
+      "",
+  type: "",
+});
+let cropper111 = ref(null);
+
+const uploadImage = function (e: any) {
+  const { files } = e.target;
+  if (files && files[0]) {
+    if (img.value.src) {
+      URL.revokeObjectURL(img.value.src);
+    }
+    const blob = URL.createObjectURL(files[0]);
+    img.value.src = blob;
+    img.value.type= files[0].type;
+  }
+}
+const crop=function () {
+  if (cropper111.value) {
+    const result = cropper111.value.getResult();
+    img.value.src=result.canvas.toDataURL(img.value.type);
+  }
+}
+</script>
+
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <h1>広告画像提出フォーム</h1>
+    <div v-show="img.src.length===0">画像が選択されていません。</div>
+    <cropper
+        v-show="img.src.length!==0"
+        ref="cropper111"
+        class="cropper"
+        :src="img.src"
+        :stencil-props="{aspectRatio: 3}"
+    />
+    <button class="button" @click="$refs.file.click()">
+      <input
+          type="file"
+          ref="file"
+          @change="uploadImage($event)"
+          accept="image/*"
+      />
+      画像を選択する
+    </button>
+    <button class="button" @click="crop">画像を切り取る</button>
   </div>
 </template>
-
-<script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
-@Options({
-  components: {
-    HelloWorld,
-  },
-})
-export default class HomeView extends Vue {}
-</script>
+<style lang="scss">
+.button {
+  input {
+    display: none;
+  }
+}
+.cropper {
+  max-height: 500px;
+}
+</style>
