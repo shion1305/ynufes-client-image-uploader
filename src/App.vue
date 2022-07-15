0bcdcc032a49dc459653f54012aed740e85d2751
Shion Ichikawa <shion1305@gmail.com>
2022-07-15 17:06:23 +0900

    💄 Attributionとcopyright表記の追加

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <router-view/>
+  <div class="copyright">Copyright ©2022 横浜国立大学 大学祭実行委員会</div>
+    <a href="https://www.flaticon.com" title="icons attribution">Icons Attribution: Flaticon</a>
 </template>
 
 <style>
@@ -22,5 +24,9 @@ nav a {
 
 nav a.router-link-exact-active {
   color: #42b983;
+}
+
+.copyright{
+  margin-top: 2rem;
 }
 </style>
